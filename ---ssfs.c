661b3854cc74cd4fdbcb8052b31449925c386889
belladewusa <55181780+belladewusa@users.noreply.github.com>
2020-04-24 20:10:14 +0800

    Add files via upload

---
+++ ---ssfs.c
@@ -0,0 +1,160 @@
+#define FUSE_USE_VERSION 28 
+#include <fuse.h> 
+#include <sys/time.h> 
+#include <stdio.h> 
+#include <errno.h> 
+#include <string.h> 
+#include <dirent.h> 
+#include <unistd.h> 
+#include <fcntl.h>
+
+
+static const char *dirpath = "/home/nabella/shift4/Documents"; 	  
+
+9(ku@AW1[Lmvgax6q`5Y2Ry?+sF!^HKQiBXCUSe&0M.b%rI'7d)o4~VfZ*{#:}ETt$3J-zpc]lnh8,GwP_ND|jO
+void write_log()
+
+static int xmp_mkdir(const char *path, mode_t mode)
+	{
+	int res; 	
+    res = mkdir(fpath,mode); 
+    if(res==1)
+		{
+	    return -errno; 
+		}
+	return 0; 
+	}
+
+static  int  xmp_getattr(const char *path, struct stat *stbuf)
+	{	
+	int res;  
+	char fpath[1000];  
+	sprintf(fpath,"%s%s",dirpath,path); 
+	res = lstat(fpath, stbuf); 
+	if (res == -1)
+	return -errno; 
+	return 0;
+	}
+
+static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi)
+	{
+	char fpath[1000]; 
+	if(strcmp(path,"/") == 0)
+		{
+		path=dirpath;
+		sprintf(fpath,"%s",path); 
+		}
+	else sprintf(fpath, "%s%s",dirpath,path); 
+	int res = 0; 
+	DIR *dp;
+	struct dirent *de; 
+	(void) offset; 
+	(void) fi; 
+	dp = opendir(fpath); 
+	if (dp == NULL)
+	return -errno;
+	while ((de = readdir(dp)) != NULL) 
+		{
+		struct stat st;
+		memset(&st, 0, sizeof(st)); 
+		st.st_ino = de->d_ino;
+		st.st_mode = de->d_type << 12; 
+		res = (filler(buf, de->d_name, &st, 0)); 
+		if(res!=0) break;  
+		}
+	closedir(dp); 
+	return 0; 
+	}
+
+static int xmp_read(const char *path, char *buf, size_t size, off_t offset,struct fuse_file_info *fi)
+	{
+	char fpath[1000]; 
+	if(strcmp(path,"/") == 0)
+		{
+		path=dirpath; 
+		sprintf(fpath,"%s",path); 
+		}
+	else sprintf(fpath, "%s%s",dirpath,path); 
+	int res = 0; 
+	int fd = 0 ; 
+	(void) fi; 
+	fd = open(fpath, O_RDONLY); 
+	if (fd == -1)
+	return -errno; 
+	res = pread(fd, buf, size, offset);  
+	if (res == -1)
+	res = -errno; 
+	close(fd); 
+	return res;  
+	}
+
+
+static int xmp_mknod(const char *path, mode_t mode, dev_t rdev)
+	{
+	}
+
+static int xmp_rmdir(const char *path)
+	{
+    int res;
+    if(res==-1)
+    return -errno;   
+    return 0;
+	}
+
+static int xmp_rename(const char *from, const char *to)
+	{
+    if(res == -1)
+	return -errno; 
+    return 0; 
+	}
+
+static int xmp_truncate(const char *path, off_t size)
+	{
+	}
+
+static int xmp_write(const char *path, const char *buf, size_t size, off_t offset, struct fuse_file_info *fi)
+	{
+    int fd;   
+    int res; 
+    (void) fi;
+    fd = open(fpath, O_WRONLY);         
+    if (fd == -1)
+    return -errno;  
+    res = pwrite(fd, buf, size, offset);
+    if (res == -1)
+    res = -errno; 
+    close(fd); 
+    return res;     
+	}
+	
+static int xmp_unlink(const char *path)
+	{
+    int res;	    
+    res = unlink(fpath); 
+    if(res == -1)
+	return -errno; 
+    return 0; 
+	}	     
+	
+
+static struct fuse_operations xmp_oper = 
+	{
+		.getattr = xmp_getattr,
+		.readdir = xmp_readdir,
+		.read = xmp_read,
+		.mkdir = xmp_mkdir,
+		.rmdir = xmp_rmdir,
+		.mknod = xmp_mknod,
+		.rename = xmp_rename,
+		.write = xmp_write,
+		.unlink = xmp_unlink,
+		.truncate = xmp_truncate,
+
+	}; 
+
+
+int  main(int  argc, char *argv[])
+	{
+		umask(0);  
+		return fuse_main(argc, argv, &xmp_oper, NULL);  
+	}
